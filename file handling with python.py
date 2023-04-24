b5f368d11103f7bde4172a488400a88d88d46b54
githunter9393 <129621097+githunter9393@users.noreply.github.com>
2023-04-24 09:34:32 +0300

    Create file handling with python.py
    this program reads two files.  it counts the lines in the first file, if the number is odd: the odd lines are copied into the second file. and a fitting message is displayed.
    otherwise -  it will only display the content of the files.

---
+++ file handling with python.py
@@ -0,0 +1,56 @@
+
+# opening the file
+file1 = open('file1.txt', 'r')
+
+# creating another file to store odd lines
+file2 = open('file2.txt', 'w')
+
+# reading content of the files
+# and writing odd lines to another file
+lines = file1.readlines()
+type(lines)
+for i in range(0, len(lines)):
+	if(i % 2 != 0):
+		file2.write(lines[i])
+
+# closing the files
+file1.close()
+file2.close()
+
+
+F=open("file1.txt","r")
+
+count=0
+
+while(True):
+    b=F.read(1)
+    if(b=='\n'):
+        count+=1
+    if(b==""):
+        break
+
+F.close()
+
+
+# opening the files and printing their content
+file1 = open('file1.txt', 'r')
+file2 = open('file2.txt', 'r')
+
+# reading and printing the files content
+str1 = file1.read()
+str2 = file2.read()
+
+print("file1 content...")
+print(str1)
+
+print() # to print new line
+
+print("file2 content...")
+print(str2)
+
+# closing the files
+file1.close()
+file2.close()
+
+if(count % 2 != 0):
+	print("Number of lines odd, content copied into file2 ")
